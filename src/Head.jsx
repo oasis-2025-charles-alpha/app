8cd5029e806593e2a56175b26f4cdbaeb1aa756f
Oscar-Kong <kong.weiyi@northeastern.edu>
2025-02-02 13:27:37 -0500

    Changed header

---
+++ src/Head.jsx
@@ -0,0 +1,14 @@
+function Head() {
+    return ( <>
+        <header>
+            <nav>
+                <button className="trending">Trending</button>
+                <button className="latest">Latest</button>
+                <button className="unread">Unread</button>
+                <button className="Newest">Newest</button>
+            </nav>
+        </header>
+    </> )
+}
+
+export default Head;
