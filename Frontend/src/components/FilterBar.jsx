1f8a7a935a7cd89d0263d7a7314e4822dba97c94
XiaobinMei <mei.xiaob@northeastern.edu>
2025-03-09 13:13:17 -0400

    change the position of filters

--- Frontend/src/components/FilterBar.jsx
+++ Frontend/src/components/FilterBar.jsx
@@ -47,6 +47,25 @@ function FilterBar({
                 </div>
             </div>
 
+            {/* College Filter Section */}
+            <div className="college-filter">
+                <h3>College</h3>
+                <select 
+                    value={selectedCollege || ''}
+                    onChange={(e) => onCollegeChange(e.target.value)}>
+                    <option value="">All Colleges</option>
+                    <option value="Khoury College">Khoury College</option>
+                    <option value="School of Law">School of Law</option>
+                    <option value="College of Arts, Media and Design">College of Arts, Media and Design</option>
+                    <option value="D’Amore-McKim School of Business">D’Amore-McKim School of Business</option>
+                    <option value="College of Engineering">College of Engineering</option>
+                    <option value="College of Science">College of Science</option>
+                    <option value="College of Professional Studies">College of Professional Studies</option>
+                    <option value="Bouvé College of Health Sciences">Bouvé College of Health Sciences</option>
+                    <option value="College of Social Sciences and Humanities">College of Social Sciences and Humanities</option>
+                </select>
+            </div>
+
             {/* Condition Filter Section */}
             <div className="condition-filter">
                 <h3>Condition</h3>
@@ -86,25 +105,6 @@ function FilterBar({
                     />
                 </div>
                 
-            </div>
-
-            {/* College Filter Section */}
-            <div className="college-filter">
-                <h3>College</h3>
-                <select 
-                    value={selectedCollege || ''}
-                    onChange={(e) => onCollegeChange(e.target.value)}>
-                    <option value="">All Colleges</option>
-                    <option value="Khoury College">Khoury College</option>
-                    <option value="School of Law">School of Law</option>
-                    <option value="College of Arts, Media and Design">College of Arts, Media and Design</option>
-                    <option value="D’Amore-McKim School of Business">D’Amore-McKim School of Business</option>
-                    <option value="College of Engineering">College of Engineering</option>
-                    <option value="College of Science">College of Science</option>
-                    <option value="College of Professional Studies">College of Professional Studies</option>
-                    <option value="Bouvé College of Health Sciences">Bouvé College of Health Sciences</option>
-                    <option value="College of Social Sciences and Humanities">College of Social Sciences and Humanities</option>
-                </select>
             </div>
         </div>
     );
