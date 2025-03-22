358539e6b26b7bde9977cba0a5e17d718a5fb388
XiaobinMei <mei.xiaob@northeastern.edu>
2025-03-22 14:15:36 -0400

    add college name to the bookcard

--- Frontend/src/components/BookCard.jsx
+++ Frontend/src/components/BookCard.jsx
@@ -1,6 +1,5 @@
 import { useLikes } from '../context/LikesContext';
 
-
 function BookCard({ book }) {
     const { toggleLike, likedBooks } = useLikes();
     const isLiked = likedBooks.some(b => b.id === book.id);
@@ -11,16 +10,16 @@ function BookCard({ book }) {
                 ♥
             </button>
             
-            <img src={book.imageUrl} alt={book.textbook_name} className="book-image" />
+            <img src={book.textbook_image} alt={book.textbook_name} className="book-image" />
             <div className="book-details">
                 <h3 className="book-title">{book.textbook_name}</h3>
                 <p className="book-author">{book.textbook_author}</p>
                 <div className="price-condition">
-                <span className="book-price">${book.textbook_price}</span>
-                <span className="course-subject">{book.course_subject} {book.course_number}</span>
-            </div>
-                <div className="course-info">
-                    <span>{book.course_subject} {book.course_number}</span>
+                    <span className="book-price">${book.textbook_price}</span>
+                </div>
+                <div className="course-college-info">
+                    <span className="course-info">{book.course_subject} {book.course_number}</span>
+                    <span className="college-name">{book.college_name}</span>
                 </div>
                 <div className="professor-info">
                     Professor: {book.professor_fname} {book.professor_lname}
